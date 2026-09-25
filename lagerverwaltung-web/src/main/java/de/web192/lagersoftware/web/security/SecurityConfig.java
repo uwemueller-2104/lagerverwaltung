a3ec1f53646de09abf361c04150cf95a2262789e
BigPhoot <uwe2104@googlemail.com>
2026-09-25 12:44:15 +0200

    fixed logo for login

--- lagerverwaltung-web/src/main/java/de/web192/lagersoftware/web/security/SecurityConfig.java
+++ lagerverwaltung-web/src/main/java/de/web192/lagersoftware/web/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/css/**", "/h2-console/**", "/api/health").permitAll()
+                        .requestMatchers("/login", "/css/**","/img/**", "/h2-console/**", "/api/health").permitAll()
                         .requestMatchers("/lager/**", "/lagerplatz/**", "/material/**",
                                 "/projekt/**", "/angebot/**", "/lieferschein/**", "/benutzer/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
